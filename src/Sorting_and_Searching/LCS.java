20b0d3b353fb343e4203f94eb99478f3f1c05a9f
Munna <Munna@DESKTOP-P5ECA4J>
2018-07-28 01:41:02 +0600

    This is a repository of some algorithms' code.

--- src/Sorting_and_Searching/LCS.java
+++ src/Sorting_and_Searching/LCS.java
@@ -1,10 +1,49 @@
 package Sorting_and_Searching;
 import java.util.*;
 public class LCS {
-    static int LCS(char[] x,char[] y,int m,int n){
+    static void lcs(String X, String Y, int m, int n){
+        
+        int[][] L = new int[m+1][n+1];
+        
+        for (int i=0; i<=m; i++){
+            for (int j=0; j<=n; j++){
+                if (i == 0 || j == 0)
+                    L[i][j] = 0;
+                else if (X.charAt(i-1) == Y.charAt(j-1))
+                    L[i][j] = L[i-1][j-1] + 1;
+                else
+                    L[i][j] = Math.max(L[i-1][j], L[i][j-1]);
+            }
+        }
+        int index = L[m][n];
+        int temp = index;
+        
+        char[] lcs = new char[index+1];
+        lcs[index] = ' ';
+  
+        int i = m, j = n;
+        while (i > 0 && j > 0){
+            if (X.charAt(i-1) == Y.charAt(j-1)){
+                lcs[index-1] = X.charAt(i-1); 
+                i--; 
+                j--; 
+                index--;     
+            }
+            else if (L[i-1][j] > L[i][j-1])
+                i--;
+            else
+                j--;
+        }
+        System.out.print("\nLCS of "+X+" and "+Y+" is ");
+        for(int k=0;k<=temp;k++)
+            System.out.print(lcs[k]);
+        System.out.println();
+    }
+    
+    static int LCS(String x,String y,int m,int n){
         if(m==0 || n==0)
             return 0;
-        if(x[m-1]==y[n-1])
+        if(x.charAt(m-1)==y.charAt(n-1))
             return 1+LCS(x,y,m-1,n-1);
         else
             return judge(LCS(x,y,m,n-1),LCS(x,y,m-1,n));
@@ -12,19 +51,19 @@ public class LCS {
     static int judge(int a,int b){
         return a>b?a:b;
     }
+    
     public static void main(String [] args){
         Scanner s = new Scanner(System.in);
-        String s1,s2;
+        String x,y,s1,s2;
         System.out.print("First string: ");
-        s1=s.nextLine();
+        x=s.nextLine();
         System.out.print("Second string: ");
-        s2=s.nextLine();
+        y=s.nextLine();
         
-        char[] x=s1.toCharArray();
-        char[] y=s2.toCharArray();
-        int m=x.length;
-        int n=y.length;
+        int m=x.length();
+        int n=y.length();
         
+        lcs(x, y, m, n);
         System.out.println("LCS length: "+LCS(x,y,m,n));
     }
 }
